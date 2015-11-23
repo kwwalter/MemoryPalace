d9c2dbba7de8f8095a0dbdf72386401b0db2c875
Kevin Walter <kevinwalter0731@gmail.com>
2015-11-23 17:44:45 -0500

    fixed the dang error

--- public/js/app.js
+++ public/js/app.js
@@ -1,1 +1,20 @@
-alert('yo'); 
+alert('yo, testing!');
+
+var app = angular.module('MemoryPalace', ['ngRoute']);
+
+app.controller('MainController', ['$http', function($http){
+  this.name = "MainController";
+}]);
+
+app.config(['$routeProvider', '$locationProvider', function($routeProvider, $locationProvider){
+  $locationProvider.html5Mode({ enabled: true });
+  $routeProvider.
+    when('/', {
+      templateUrl: 'partials/welcome.html',
+      controller: 'MainController',
+      controllerAs: 'mainCtrl'
+    }).
+    otherwise({
+      redirectTo: '/'
+    });
+}]);
