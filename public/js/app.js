9fe9cb680b82e983171c2a8a227544fa99d55983
Kevin Walter <kevinwalter0731@gmail.com>
2015-11-23 23:17:12 -0500

    working on redirect after login or signup

--- public/js/app.js
+++ public/js/app.js
@@ -1,6 +1,6 @@
 var app = angular.module('MemoryPalace', ['ngRoute']);
 
-app.controller('MainController', ['$http', function($http){
+app.controller('MainController', ['$http', '$window', function($http, $window){
   var controller = this;
 
   this.signup = function() {
@@ -12,23 +12,38 @@ app.controller('MainController', ['$http', function($http){
       password: controller.password
     }).then(function(data){
       console.log(data);
+      if (data.data._id) {
+        $window.location.href = '/' + data.data._id + '/loggedin';
+      } else {
+        $('body').append('<h2>Sorry, there was an error signing you up--try again!</h2>');
+      }
     }, function(error){
       console.log("there was an error: ", error);
     });
   };
 
   this.login = function() {
-    alert('loggin!');
+    // alert('loggin!');
     $http.post('/login', {
       userEmail: controller.userEmail,
       password: controller.password
     }).then(function(data){
       console.log(data);
+      if (data.data._id) {
+        $window.location.href = '/' + data.data._id + '/loggedin';
+      } else {
+        $('body').append('<h2>Sorry, there was an error logging you in--try again!</h2>');
+      }
     }, function(error){
       console.log("there was an error: ", error);
     });
   };
+}]);
 
+app.controller('LoggedInController', ['$http', '$window', '$routeParams', function($http, $window, $routeParams){
+  var controller = this;
+
+  this.userEmail = $routeParams.userEmail;
 }]);
 
 app.config(['$routeProvider', '$locationProvider', function($routeProvider, $locationProvider){
@@ -48,6 +63,11 @@ app.config(['$routeProvider', '$locationProvider', function($routeProvider, $loc
       templateUrl: 'partials/login.html',
       controller: 'MainController',
       controllerAs: 'mainCtrl'
+    }).
+    when('/:id/loggedin', {
+      templateUrl: 'partials/loggedin.html',
+      controller: 'LoggedInController',
+      controllerAs: 'loggedinCtrl'
     }).
     otherwise({
       redirectTo: '/'
